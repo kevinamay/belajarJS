ca995d28fb8b2b6fbde999bb4b702004b40557f4
kevinamay <kevinamay23@gmail.com>
2024-03-17 16:49:33 +0700

    commit

---
+++ main.js
@@ -0,0 +1,61 @@
+// alert("kamu wumbo?");
+// console.log("kamu wumbo?");
+//bedanya alert dan console laog adalah jika alert akan muncul popup , console log munculnya di inspeksi elemen di console
+
+// prompt("masukkan pendapat anda tentang wumbo :");
+// prompt itu pop up yang bisa diisi
+
+// untuk menentukan variabel ada banyak cara :
+// const x = 1
+// let y = 2
+// var usia = 30
+// console.log('kamu wumbo?'+ usia);
+// const itu nilainya tidak bisa dirubah , kalau let/var bisa dirubah
+
+// CONTOH
+// let usia = 30;
+// usia = 25;
+// console.log("kamu wumbo?" + usia);
+// alert('usia wumbo?'+usia)
+
+// let pendapat= prompt("masukkan pendapat anda tentang wumbo :");
+// alert('pendapat wumbo adalah'+ pendapat )
+
+// let jenis = "wumbo 1";
+// let usai = 25;
+// let tinggiBadan = 180.0;
+// let beratBadan;
+// let citaCita = "polisi";
+// beratBadan = 100;
+
+// if (citaCita == null) {
+//   citaCita = "tidak ada cita cita";
+// } else {
+//   citaCita = "ada cita cita";
+// }
+
+// let umurSebelum = 10;
+// let umurSesudah = 30;
+// const sisaUmur = 100;
+// const umurBanding = sisaUmur - umurSesudah - umurSebelum;
+
+// alert('jenis wumbo ini adalah :'+ jenis +'usia wumbo'+ usai)
+// alert(
+//   `jenis wumbo ${jenis} usia wumbo ${usai} tinggi badan wumbo ${tinggiBadan} berat badan wumbo ${beratBadan} cita cita wumbo ${citaCita}`
+// );
+// alert(
+//   `umur wumbo sebelum ${umurSebelum} & umur sesudah wumbo ${umurSesudah}, jadi perbedaan umurnya adalah ${umurBanding}`
+// );
+
+// ARRAY
+let namaSiswa = ["bambang", "joko", "budi"];
+// cara menamvahkan namasiswa
+namaSiswa.push("doni", "bimo");
+namaSiswa[5] = "dika";
+// menghapus index awal
+namaSiswa.shift();
+// menghapus index akhir
+namaSiswa.pop();
+alert(namaSiswa);
+
+// FOR LOOP
